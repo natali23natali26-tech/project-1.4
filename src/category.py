7c76165dd7bf609a7f3299e64988d830e5f7b8f3
Natalie Gavrilova <natali23natali26-tech@users.noreply.github.com>
2025-12-18 23:33:49 +0300

    correct category

--- src/category.py
+++ src/category.py
@@ -39,3 +39,14 @@ class Category:
     def __repr__(self) -> str:
         return (f"Category(name='{self.name}', "
                 f"products_count={len(self.__products)})")
+
+    def average_price(self) -> float:
+        """
+        Возвращает среднюю цену товаров в категории.
+        Если товаров нет — возвращает 0.
+        """
+        try:
+            total_price = sum(product.price for product in self.__products)
+            return total_price / len(self.__products)
+        except ZeroDivisionError:
+            return 0.0
